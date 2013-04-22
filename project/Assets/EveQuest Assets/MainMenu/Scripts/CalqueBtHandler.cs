b1f77a675e2b3378a7c680859a9b9ed4188845d4
Jonathan Ouchy <jobesu14@gmail.com>
2013-04-22 11:29:00 +0200

    Loading panel added

--- project/Assets/EveQuest Assets/MainMenu/Scripts/CalqueBtHandler.cs
+++ project/Assets/EveQuest Assets/MainMenu/Scripts/CalqueBtHandler.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 public class CalqueBtHandler : MonoBehaviour {
-
+	
+	public GameObject itemsPanel;
+	public GameObject loadingPanel;
+	
 	void OnClick() {
 		Debug.Log("Calque bt pressed");
+		
+		Destroy( itemsPanel );
+		loadingPanel.SetActiveRecursively( true );
 		
 		PlayerPrefs.SetInt( "show_mask", 1);
 		Application.LoadLevel("Loupe");
--- project/Assets/EveQuest Assets/MainMenu/Scripts/LoupeBtHandler.cs
+++ project/Assets/EveQuest Assets/MainMenu/Scripts/LoupeBtHandler.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 public class LoupeBtHandler : MonoBehaviour {
+	
+	public GameObject itemsPanel;
+	public GameObject loadingPanel;
 
 	void OnClick() {
 		Debug.Log("Loupe bt pressed");
+		
+		Destroy( itemsPanel );
+		loadingPanel.SetActiveRecursively( true );
 		
 		PlayerPrefs.SetInt( "show_mask", 0);
 		Application.LoadLevel("Loupe");
